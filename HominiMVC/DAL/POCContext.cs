b1145e38539f8ebdf52688466b49d3d4010cb4d1
Alejo Rizo <arizomo@gmail.com>
2019-05-22 20:18:30 -0500

    with migrations

---
+++ HominiMVC/DAL/POCContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace HominiMVC.DAL
+{
+    public class POCContext : DbContext
+    {
+        public POCContext() : base("POCContext")
+        {
+
+        }
+    }
+}
---
+++ HominiMVC/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace HominiMVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HominiMVC.DAL.POCContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(HominiMVC.DAL.POCContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
